0d0082803a88f1c56e726bf3801842f9bb8b4760
Suvendu Hatua <suvenduhatua01@gmail.com>
2024-12-18 20:48:49 +0530

    Adding Store Entity and all the Setter and Getter methods

---
+++ src/main/java/com/spring_boot/rest_service/Rest_Api_Redis_Cache/entity/Store.java
@@ -0,0 +1,95 @@
+package com.spring_boot.rest_service.Rest_Api_Redis_Cache.entity;
+
+import jakarta.persistence.*;
+
+import java.util.Date;
+
+@Entity
+@Table(name = "store")
+public class Store {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
+    private int id;
+    @Column(name = "product_name",nullable = false)
+    private String productName;
+
+    @Column(name = "product_desc")
+    private String productDesc;
+
+    @Column(name = "price",nullable = false)
+    private Double productPrice;
+
+    @Column(name = "count",nullable = false)
+    private int productCount;
+
+    @Column(name = "production_date")
+    private Date productionDate;
+
+    //Constructors
+
+    public Store() {
+    }
+
+    public Store(String productName, String productDesc, Double productPrice, int productCount, Date productionDate) {
+        this.productName = productName;
+        this.productDesc = productDesc;
+        this.productPrice = productPrice;
+        this.productCount = productCount;
+        this.productionDate = productionDate;
+    }
+    //Setter and getter
+
+
+    public String getProductName() {
+        return productName;
+    }
+
+    public void setProductName(String productName) {
+        this.productName = productName;
+    }
+
+    public String getProductDesc() {
+        return productDesc;
+    }
+
+    public void setProductDesc(String productDesc) {
+        this.productDesc = productDesc;
+    }
+
+    public Double getProductPrice() {
+        return productPrice;
+    }
+
+    public void setProductPrice(Double productPrice) {
+        this.productPrice = productPrice;
+    }
+
+    public int getProductCount() {
+        return productCount;
+    }
+
+    public void setProductCount(int productCount) {
+        this.productCount = productCount;
+    }
+
+    public Date getProductionDate() {
+        return productionDate;
+    }
+
+    public void setProductionDate(Date productionDate) {
+        this.productionDate = productionDate;
+    }
+
+    @Override
+    public String toString() {
+        return "Store{" +
+                "id=" + id +
+                ", productName='" + productName + '\'' +
+                ", productDesc='" + productDesc + '\'' +
+                ", productPrice=" + productPrice +
+                ", productCount=" + productCount +
+                ", productionDate=" + productionDate +
+                '}';
+    }
+}
