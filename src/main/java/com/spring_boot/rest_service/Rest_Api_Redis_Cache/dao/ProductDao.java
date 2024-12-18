107b5a5c9350d5f1bb176a75f7a6db6a9b224db1
Suvendu Hatua <suvenduhatua01@gmail.com>
2024-12-18 22:24:56 +0530

    updating ProductDao

--- src/main/java/com/spring_boot/rest_service/Rest_Api_Redis_Cache/dao/ProductDao.java
+++ src/main/java/com/spring_boot/rest_service/Rest_Api_Redis_Cache/dao/ProductDao.java
@@ -1,18 +1,16 @@
 package com.spring_boot.rest_service.Rest_Api_Redis_Cache.dao;
 
 import com.spring_boot.rest_service.Rest_Api_Redis_Cache.entity.Product;
+import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.List;
 
 public interface ProductDao {
-    public List<Product> getProducts();
-    public Product getProductById(int id);
+
     public List<Product> searchProductByName(String name);
     public List<Product> sortProductsByPriceAsc();
     public List<Product> sortProductsByPriceDesc();
     public List<Product> searchProductsByProductionYear(String year);
-    public Product addProduct(Product product);
-    public Product updateProduct(int id, Product product);
-    public void deleteProductById(int id);
+
 
 }
