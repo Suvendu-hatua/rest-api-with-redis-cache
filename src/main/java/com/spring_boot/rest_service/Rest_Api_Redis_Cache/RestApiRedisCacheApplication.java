9058c7a1b441cc551964d5bf68de5ce519dde3c8
Suvendu Hatua <suvenduhatua01@gmail.com>
2024-12-18 20:37:32 +0530

    Adding all the initial files & folders

---
+++ src/main/java/com/spring_boot/rest_service/Rest_Api_Redis_Cache/RestApiRedisCacheApplication.java
@@ -0,0 +1,13 @@
+package com.spring_boot.rest_service.Rest_Api_Redis_Cache;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestApiRedisCacheApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RestApiRedisCacheApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/spring_boot/rest_service/Rest_Api_Redis_Cache/RestApiRedisCacheApplicationTests.java
@@ -0,0 +1,13 @@
+package com.spring_boot.rest_service.Rest_Api_Redis_Cache;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RestApiRedisCacheApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
