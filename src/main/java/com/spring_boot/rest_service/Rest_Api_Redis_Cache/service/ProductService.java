1907e15dc12449cbde47901c476cca838396ecaf
Suvendu Hatua <suvenduhatua01@gmail.com>
2024-12-18 22:39:26 +0530

    Adding all the required methods to ProductService

--- src/main/java/com/spring_boot/rest_service/Rest_Api_Redis_Cache/service/ProductService.java
+++ src/main/java/com/spring_boot/rest_service/Rest_Api_Redis_Cache/service/ProductService.java
@@ -1,4 +1,17 @@
 package com.spring_boot.rest_service.Rest_Api_Redis_Cache.service;
 
+import com.spring_boot.rest_service.Rest_Api_Redis_Cache.entity.Product;
+
+import java.util.List;
+
 public interface ProductService {
+    public List<Product> getProducts();
+    public Product getProductById(int id);
+    public List<Product> searchProductByName(String name);
+    public List<Product> sortProductsByPriceAsc();
+    public List<Product> sortProductsByPriceDesc();
+    public List<Product> searchProductsByProductionYear(String year);
+    public Product addProduct(Product product);
+    public Product updateProduct(int id, Product product);
+    public void deleteProductById(int id);
 }
