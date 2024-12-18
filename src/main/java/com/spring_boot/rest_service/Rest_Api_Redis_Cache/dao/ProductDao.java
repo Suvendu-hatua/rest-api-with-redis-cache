bce87785c3e9fe009478013f8cfd19aecc18e8cb
Suvendu Hatua <suvenduhatua01@gmail.com>
2024-12-18 21:01:23 +0530

    Adding ProductDao and it's all un-implemented methods()

---
+++ src/main/java/com/spring_boot/rest_service/Rest_Api_Redis_Cache/dao/ProductDao.java
@@ -0,0 +1,18 @@
+package com.spring_boot.rest_service.Rest_Api_Redis_Cache.dao;
+
+import com.spring_boot.rest_service.Rest_Api_Redis_Cache.entity.Product;
+
+import java.util.List;
+
+public interface ProductDao {
+    public List<Product> getProducts();
+    public Product getProductById(int id);
+    public List<Product> searchProductByName(String name);
+    public List<Product> sortProductsByPriceAsc();
+    public List<Product> sortProductsByPriceDesc();
+    public List<Product> searchProductsByProductionYear(String year);
+    public Product addProduct(Product product);
+    public Product updateProduct(int id, Product product);
+    public void deleteProductById(int id);
+
+}
--- src/main/java/com/spring_boot/rest_service/Rest_Api_Redis_Cache/dao/StoreDao.java
+++
@@ -1,7 +0,0 @@
-package com.spring_boot.rest_service.Rest_Api_Redis_Cache.dao;
-
-import com.spring_boot.rest_service.Rest_Api_Redis_Cache.entity.Product;
-
-public interface StoreDao {
-    public List<Product> get
-}
