1c954f55af93520604a07d4136f333d725103342
Suvendu Hatua <suvenduhatua01@gmail.com>
2024-12-19 20:11:16 +0530

    Enabling whole spring boot application as Cacheable by annotating @EnableChaching

--- src/main/java/com/spring_boot/rest_service/Rest_Api_Redis_Cache/RestApiRedisCacheApplication.java
+++ src/main/java/com/spring_boot/rest_service/Rest_Api_Redis_Cache/RestApiRedisCacheApplication.java
@@ -2,8 +2,10 @@ package com.spring_boot.rest_service.Rest_Api_Redis_Cache;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cache.annotation.EnableCaching;
 
 @SpringBootApplication
+@EnableCaching
 public class RestApiRedisCacheApplication {
 
 	public static void main(String[] args) {
